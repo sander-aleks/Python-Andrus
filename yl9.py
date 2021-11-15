45562a1192be45957c27af0dd7e0cd4ee920014c
Sander-Aleks Mander <msanderaleks@gmail.com>
2021-11-15 11:16:16 +0200

    ma tan surra

---
+++ yl9.py
@@ -0,0 +1,15 @@
+a = int(input("sisesta midagi vaartus: "))
+b = int(input("sisesta midagi vartus: "))
+c = int(input("sisesa aluse vaartus: "))
+
+if a + b <= c or b + c <= a or a + c <= b:
+    print("sellist kolmnurka ei eksiteeri")
+
+elif a == b and b == c:
+    print("tegu on vordkygse kolmurgaga")
+
+elif a == b or b ==c or a == c
+    print("tegu on vordhaarse kolmnurgaga")
+
+else:
+    print("Tegu on erikylgse kolmnurgaga")
